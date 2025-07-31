87c532ea7c5ee6db3c136732c969c23f076d2257
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-07-31 18:45:30 +0300

    Added Ui elements to manus

--- menu_main.js
+++ menu_main.js
@@ -7,25 +7,41 @@ window.onload = function() {
 	playBtn.src="sprites/menu/playBtn.png";
 	playBtnHover=new Image();
 	playBtnHover.src="sprites/menu/playBtnHover.png";
+	backToMenu= new Image();
+	backToMenu.src="sprites/menu/backToMenuBtn.png";
+	backToMenuHover= new Image();
+	backToMenuHover.src="sprites/menu/backToMenuHover.png";
+	playLevel=new Image();
+	playLevel.src="sprites/menu/playLevelBtn.png";
+	playLevelHover=new Image();
+	playLevelHover.src="sprites/menu/playLevelHover.png";
+	prevBtn=new Image();
+	prevBtn.src="sprites/menu/prevBtn.png";
+	prevHover=new Image();
+	prevHover.src="sprites/menu/prevHover.png";
+	nextBtn=new Image();
+	nextBtn.src="sprites/menu/nextBtn.png";
+	nextHover=new Image();
+	nextHover.src="sprites/menu/nextHover.png"; 
 	
 	title_screen_button_list=[
-		createMenuButton(760,100,80,50,function(){menu.changeButtonList(level_select_button_list)},
+		createMenuButton(window.innerWidth/2-160,window.innerHeight/2-40,320,80,function(){menu.changeButtonList(level_select_button_list)},
 			function(x,y){co.drawImage(playBtn,x,y,320,80)},
 			function(x,y){co.drawImage(playBtnHover,x,y,320,80)},),
 	]
 	level_select_button_list=[
-		createMenuButton(650,0,320,50,function(){menu.changeButtonList(title_screen_button_list)},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="black";co.fillText("back to main menu",x,y,320)},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="blue";co.fillText("back to main menu",x,y,320)}),
-		createMenuButton(700,100,80,50,function(){if(selected_level>1){selected_level--}},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="black";co.fillText("prev",x,y,80)},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="blue";co.fillText("prev",x,y,80)}),
-		createMenuButton(820,100,80,50,function(){if(selected_level<MAX_LEVEL){selected_level++}},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="black";co.fillText("next",x,y,80)},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="blue";co.fillText("next",x,y,80)}),
-		createMenuButton(690,200,240,50,function(){window.location.href="./canvas.html?lvl="+selected_level},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="black";co.fillText("play level "+selected_level,x,y,240)},
-			function(x,y){co.textBaseline="top";co.font="50px Arial";co.fillStyle="blue";co.fillText("play level "+selected_level,x,y,240)}),
+		createMenuButton(650,50,320,80,function(){menu.changeButtonList(title_screen_button_list)},
+			function(x,y){co.drawImage(backToMenu,x,y,320,80)},
+			function(x,y){co.drawImage(backToMenuHover,x,y,320,80)},),
+		createMenuButton(650,150,160,80,function(){if(selected_level>1){selected_level--}},
+			function(x,y){co.drawImage(prevBtn,x,y,160,80)},
+			function(x,y){co.drawImage(prevHover,x,y,160,80)},),
+		createMenuButton(850,150,160,80,function(){if(selected_level<MAX_LEVEL){selected_level++}},
+			function(x,y){co.drawImage(nextBtn,x,y,160,80)},
+			function(x,y){co.drawImage(nextHover,x,y,160,80)},),
+		createMenuButton(650,250,320,80,function(){window.location.href="./canvas.html?lvl="+selected_level},
+			function(x,y){co.drawImage(playLevel,x,y,320,80)},
+			function(x,y){co.drawImage(playLevelHover,x,y,320,80)},),
 	]
 	menu.changeButtonList(title_screen_button_list)
 	onmousemove=function(e){menu.onMouseMoveSomewhere(e.pageX*c.width/window.innerWidth,e.pageY*c.height/window.innerHeight)}
